dd1c3f0a746f2c764b631e06797ac2cca8dc92a6
Stephen Clarke <44792415+sdclarkelab@users.noreply.github.com>
2021-06-27 14:20:03 -0500

    feat: create links function

--- src/controllers/stockController.js
+++ src/controllers/stockController.js
@@ -6,13 +6,12 @@ const stockModel = mongodbConfig.getStockModel();
 
 exports.getStocks = async (req, res, next) => {
   try {
-    const options = StockService.createOptions(req);
     const filter = StockService.createFindAllFilter(req);
 
     const stocks = await stockModel.find(filter)
-      .select(options.fields)
-      .limit(options.limit)
-      .skip(options.limit * options.pageNumber)
+      .select(req.options.fields)
+      .limit(req.options.limit)
+      .skip(req.options.limit * req.options.pageNumber)
       .lean({ virtuals: true })
       .exec();
 
@@ -26,22 +25,11 @@ exports.getStocks = async (req, res, next) => {
 
 exports.getStock = async (req, res, next) => {
   try {
-    const options = StockService.createOptions(req);
-
     const stock = await stockModel.findOne(StockService.createFindOneFilter(req))
-      .select(options.fields)
+      .select(req.options.fields)
       .lean({ virtuals: true });
 
     return res.status(200).json(StockViewModel.createStockViewModel(stock));
-  } catch (error) {
-    return next(error);
-  }
-};
-
-exports.extractQueryParams = (req, res, next) => {
-  try {
-    req.params = StockService.createOptions(req);
-    return next();
   } catch (error) {
     return next(error);
   }
--- src/services/stockService.js
+++ src/services/stockService.js
@@ -2,27 +2,7 @@ const _ = require('lodash');
 
 const StockService = (function () {
   // ************** Private Functions **************
-
-  // ************** Private Functions **************
   return {
-    createOptions: (req) => {
-      const queryParams = req.query;
-
-      const limit = _.get(queryParams, 'limit', 0) > 0 ? parseInt(queryParams.limit, 10) : 10;
-      const fields = _.get(queryParams, 'fields', '');
-      const pageNumber = parseInt(_.get(queryParams, 'offset', 0), 10);
-      const search = _.get(queryParams, 'search', '');
-
-      const options = {
-        limit,
-        fields,
-        pageNumber,
-        search,
-      };
-
-      return options;
-    },
-
     createFindAllFilter: (req) => {
       const symbols = _.get(req.query, 'symbols') ? _.get(req.query, 'symbols', '').replace(/\s/g, '').split(',') : [];
 
